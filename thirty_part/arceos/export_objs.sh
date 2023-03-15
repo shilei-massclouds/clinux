09dfa4558f7147f52b9fb46745c2909e0c22feb7
Shi Lei <shi_lei@massclouds.com>
2023-03-15 11:58:00 +0800

    arceos: update patch & script for memtest

--- thirty_part/arceos/export_objs.sh
+++ thirty_part/arceos/export_objs.sh
@@ -3,13 +3,16 @@
 SRC_DIR=./target/riscv64gc-unknown-none-elf/release/deps/
 DST_DIR=../clinux/extern_rlib/
 
-for com in axhal axlog axruntime compiler_builtins core \
-    libax log memory_addr riscv arceos_helloworld ;
+for com in axhal axlog axruntime axalloc \
+    compiler_builtins core alloc allocator \
+    page_table_entry page_table \
+    buddy_system_allocator bitmap_allocator \
+    libax log memory_addr riscv arceos_helloworld arceos_memtest ;
 do
+    rm -rf $DST_DIR/$com
     mkdir -p $DST_DIR/$com
     cp $SRC_DIR/lib$com-*.rlib $DST_DIR/$com
     cd $DST_DIR/$com
-    rm -f *.o
     ar -x ./lib$com-*.rlib
     pwd
     cd -
