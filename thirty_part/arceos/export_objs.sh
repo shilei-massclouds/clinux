880ced9e37626b35895ac219e1427feffacdceec
Shi Lei <shi_lei@massclouds.com>
2023-03-16 12:25:40 +0800

    arceos:multitask fix patch & script for arceos

--- thirty_part/arceos/export_objs.sh
+++ thirty_part/arceos/export_objs.sh
@@ -3,11 +3,13 @@
 SRC_DIR=./target/riscv64gc-unknown-none-elf/release/deps/
 DST_DIR=../clinux/extern_rlib/
 
-for com in axhal axlog axruntime axalloc \
+for com in axhal axlog axruntime axalloc axtask \
     compiler_builtins core alloc allocator \
     page_table_entry page_table \
     buddy_system_allocator bitmap_allocator \
-    libax log memory_addr riscv arceos_helloworld arceos_memtest ;
+    libax log memory_addr riscv \
+    arceos_helloworld arceos_memtest \
+    arceos_multitask ;
 do
     rm -rf $DST_DIR/$com
     mkdir -p $DST_DIR/$com
