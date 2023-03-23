343d9aed0fa87643b2707e8624abfc704252ce18
Shi Lei <shi_lei@massclouds.com>
2023-03-23 11:21:12 +0800

    axlog: add it

---
+++ rust_depot/axlog/src/init.c
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-2.0-only
+
+int
+init_module(void)
+{
+    //printk("module[axlog]: init begin ...\n");
+    //printk("module[axlog]: init end!\n");
+    return 0;
+}
