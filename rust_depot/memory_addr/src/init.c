a7f4fa25b07f51cb2c1539c03c5e83742737858b
Shi Lei <shi_lei@massclouds.com>
2023-03-24 14:39:14 +0800

    memory_addr: add it

---
+++ rust_depot/memory_addr/src/init.c
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-2.0-only
+
+#include "exports_memory_addr_generated.h"
+
+int
+init_module(void)
+{
+    //printk("module[memory_addr]: init begin ...\n");
+    //printk("module[memory_addr]: init end!\n");
+    return 0;
+}
