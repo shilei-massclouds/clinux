5cd15204c18a5309104fe5e1a57aeb7ca98720c3
Shi Lei <shi_lei@massclouds.com>
2025-02-09 14:32:47 +0000

    early_printk: fix loglevel

--- early_printk/src/early_printk.c
+++ early_printk/src/early_printk.c
@@ -10,11 +10,18 @@ int early_vprintk(const char *fmt, va_list args)
 {
     int n;
     char buf[512];
+    char *msg;
 
     if (!early_console) {
         booter_panic("No early_console!");
     }
 
     n = vscnprintf(buf, sizeof(buf), fmt, args);
-    early_console->write(early_console, buf, n);
+	if (printk_get_level(buf)) {
+		msg = buf + 2;
+        n -= 2;
+    } else {
+        msg = buf;
+    }
+    early_console->write(early_console, msg, n);
 }
