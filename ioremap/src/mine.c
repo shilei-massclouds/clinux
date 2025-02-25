67ed866a7af922cf1499302b7ef5f5f3685a94cc
Shi Lei <shi_lei@massclouds.com>
2025-02-25 02:20:39 +0000

    vmalloc: fix

--- ioremap/src/mine.c
+++ ioremap/src/mine.c
@@ -2,5 +2,4 @@
 #include "../../booter/src/booter.h"
 
 CL_MINE(free_vm_area)
-CL_MINE(get_vm_area_caller)
 CL_MINE(vunmap)
--- vmalloc/src/vmalloc.c
+++ vmalloc/src/vmalloc.c
@@ -2093,38 +2093,39 @@ static struct vm_struct *__get_vm_area_node(unsigned long size,
 	return area;
 }
 
-//struct vm_struct *__get_vm_area_caller(unsigned long size, unsigned long flags,
-//				       unsigned long start, unsigned long end,
-//				       const void *caller)
-//{
-//	return __get_vm_area_node(size, 1, flags, start, end, NUMA_NO_NODE,
-//				  GFP_KERNEL, caller);
-//}
-//
-///**
-// * get_vm_area - reserve a contiguous kernel virtual area
-// * @size:	 size of the area
-// * @flags:	 %VM_IOREMAP for I/O mappings or VM_ALLOC
-// *
-// * Search an area of @size in the kernel virtual mapping area,
-// * and reserved it for out purposes.  Returns the area descriptor
-// * on success or %NULL on failure.
-// *
-// * Return: the area descriptor on success or %NULL on failure.
-// */
-//struct vm_struct *get_vm_area(unsigned long size, unsigned long flags)
-//{
-//	return __get_vm_area_node(size, 1, flags, VMALLOC_START, VMALLOC_END,
-//				  NUMA_NO_NODE, GFP_KERNEL,
-//				  __builtin_return_address(0));
-//}
-//
-//struct vm_struct *get_vm_area_caller(unsigned long size, unsigned long flags,
-//				const void *caller)
-//{
-//	return __get_vm_area_node(size, 1, flags, VMALLOC_START, VMALLOC_END,
-//				  NUMA_NO_NODE, GFP_KERNEL, caller);
-//}
+struct vm_struct *__get_vm_area_caller(unsigned long size, unsigned long flags,
+				       unsigned long start, unsigned long end,
+				       const void *caller)
+{
+	return __get_vm_area_node(size, 1, flags, start, end, NUMA_NO_NODE,
+				  GFP_KERNEL, caller);
+}
+
+/**
+ * get_vm_area - reserve a contiguous kernel virtual area
+ * @size:	 size of the area
+ * @flags:	 %VM_IOREMAP for I/O mappings or VM_ALLOC
+ *
+ * Search an area of @size in the kernel virtual mapping area,
+ * and reserved it for out purposes.  Returns the area descriptor
+ * on success or %NULL on failure.
+ *
+ * Return: the area descriptor on success or %NULL on failure.
+ */
+struct vm_struct *get_vm_area(unsigned long size, unsigned long flags)
+{
+	return __get_vm_area_node(size, 1, flags, VMALLOC_START, VMALLOC_END,
+				  NUMA_NO_NODE, GFP_KERNEL,
+				  __builtin_return_address(0));
+}
+
+struct vm_struct *get_vm_area_caller(unsigned long size, unsigned long flags,
+				const void *caller)
+{
+	return __get_vm_area_node(size, 1, flags, VMALLOC_START, VMALLOC_END,
+				  NUMA_NO_NODE, GFP_KERNEL, caller);
+}
+EXPORT_SYMBOL(get_vm_area_caller);
 
 /**
  * find_vm_area - find a continuous kernel virtual area
