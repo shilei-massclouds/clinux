e12086dc483c00f7967cb57eb7d04962e3648ddb
Shi Lei <shi_lei@massclouds.com>
2025-03-03 01:27:48 +0000

    cache: riscv add it

--- vmalloc/src/init.c
+++ vmalloc/src/init.c
@@ -41,8 +41,10 @@ int __printk_ratelimit(const char *func)
 }
 EXPORT_SYMBOL(__printk_ratelimit);
 
+/*
 void flush_icache_pte(pte_t pte)
 {
     booter_panic("No impl 'slub'.");
 }
 EXPORT_SYMBOL(flush_icache_pte);
+*/
