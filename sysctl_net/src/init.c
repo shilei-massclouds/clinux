2c9884c5a9b7c88950643903c9133794708aab7d
Shi Lei <shi_lei@massclouds.com>
2025-03-04 03:12:59 +0000

    sysctl_net: add it

---
+++ sysctl_net/src/init.c
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-2.0-only
+
+#include <linux/types.h>
+#include <linux/export.h>
+#include "../../booter/src/booter.h"
+
+int
+cl_sysctl_net_init(void)
+{
+    sbi_puts("module[sysctl_net]: init begin ...\n");
+    sbi_puts("module[sysctl_net]: init end!\n");
+    return 0;
+}
+EXPORT_SYMBOL(cl_sysctl_net_init);
