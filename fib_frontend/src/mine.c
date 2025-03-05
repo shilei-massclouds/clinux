4d692a179caa4bd350902ddde993a43d17ce7a82
Shi Lei <shi_lei@massclouds.com>
2025-03-05 01:15:21 +0000

    fix

--- fib_frontend/src/mine.c
+++ fib_frontend/src/mine.c
@@ -1,14 +1,15 @@
 #include <linux/types.h>
 #include "../../booter/src/booter.h"
 
-CL_MINE(fib4_notifier_exit)
-CL_MINE(fib4_notifier_init)
 CL_MINE(nexthop_find_by_id)
 
-CL_MINE(call_fib4_notifiers)
 CL_MINE(dst_release_immediate)
 CL_MINE(fib_check_nexthop)
 CL_MINE(ip_fib_metrics_init)
 CL_MINE(nexthop_free_rcu)
 CL_MINE(tcp_ca_get_key_by_name)
-CL_MINE(call_fib4_notifier)
+
+CL_MINE(call_fib_notifiers)
+CL_MINE(call_fib_notifier)
+CL_MINE(fib_notifier_ops_unregister)
+CL_MINE(fib_notifier_ops_register)
