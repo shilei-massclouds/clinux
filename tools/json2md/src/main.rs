ac4a7b5aa6dd5586f5ecea7f1125eb64d529caa6
Shi Lei <shi_lei@massclouds.com>
2025-03-06 12:11:48 +0000

    maxlevel: fix

--- tools/json2md/src/main.rs
+++ tools/json2md/src/main.rs
@@ -1,20 +1,26 @@
 use std::env;
 use std::fs::{self, File};
 use std::io::Write;
+use std::sync::{Once, OnceLock};
 use anyhow::Result;
-use log::debug;
+use log::{debug, info};
+
+static MAX_LEVEL: OnceLock<i32> = OnceLock::new();
 
 fn main() -> Result<()> {
     env_logger::init();
 
     let args: Vec<_> = env::args().collect();
-    if args.len() != 3 {
-        panic!("Usage: json2md <profile> <target>");
+    if args.len() < 3 {
+        panic!("Usage: json2md <profile> <target> [maxlevel]");
     }
     let profile_name = &args[1];
     let target_name = &args[2];
-    debug!("Convert {} (root = {}) to mermaid.md ...",
-        profile_name, target_name);
+    if args.len() == 4 {
+        let _ = MAX_LEVEL.set((&args[3]).parse::<i32>()?);
+    }
+    debug!("Convert {} (root = {}) to mermaid.md maxlevel[{}] ...",
+        profile_name, target_name, MAX_LEVEL.get().unwrap_or(&i32::MAX));
 
     let profile = fs::read_to_string(profile_name)?;
     let mut root = json::parse(&profile)?;
@@ -23,15 +29,28 @@ fn main() -> Result<()> {
     assert!(root.has_key(target_name));
 
     let mut md = create_markdown(target_name)?;
-    output_markdown(&mut md, root, target_name)?;
+    let mut level = 0;
+    output_markdown(&mut md, root, target_name, &mut level)?;
+    assert_eq!(level, 0);
     complete_markdown(md)?;
 
     Ok(())
 }
 
 fn output_markdown(
-    f: &mut File, root: &mut json::JsonValue, name: &str
+    f: &mut File, root: &mut json::JsonValue, name: &str, level: &mut i32,
 ) -> Result<()> {
+    debug!("Current loglevel: {}", level);
+    if let Some(maxlevel) = MAX_LEVEL.get() {
+        if *level >= *maxlevel {
+            static FLAG: Once = Once::new();
+            FLAG.call_once(|| {
+                info!("Reach max level, exit ahead of time!");
+            });
+            return Ok(());
+        }
+    }
+
     if !root.has_key(name) {
         return Ok(());
     }
@@ -50,7 +69,9 @@ fn output_markdown(
 
     for child in target.members() {
         //debug!("child: {}", child);
-        output_markdown(f, root, child.as_str().unwrap())?;
+        *level += 1;
+        output_markdown(f, root, child.as_str().unwrap(), level)?;
+        *level -= 1;
     }
 
     Ok(())
