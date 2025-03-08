efe818fa0af9c8fab3366b2aa78019e7ce9c0ef1
Shi Lei <shi_lei@massclouds.com>
2025-03-08 10:48:22 +0000

    net:dev: display

--- linux_apps/init.c
+++ linux_apps/init.c
@@ -8,30 +8,40 @@
 #include <fcntl.h>
 #include <errno.h>
 
+#define LINELEN 512
+
+static void display_net_dev() {
+	int ret;
+	char line[LINELEN];
+	FILE *fstatus = NULL;
+    unsigned int lock_sz = 0;
+
+	fstatus = fopen("/proc/net/dev", "r");
+	if (fstatus == NULL)
+        printf("Open dev status failed\n");
+
+	while (fgets(line, LINELEN, fstatus) != NULL) {
+        printf("line: %s\n", line);
+    }
+
+	fclose(fstatus);
+    return 0;
+}
+
 int main(int argc, char *argv[]) {
     const char *source = "proc";
     const char *target = "/proc";
 
-    // mount proc filesystem
+    /* mount proc filesystem */
     if (mount(source, target, "proc", MS_MGC_VAL, NULL) == -1) {
         if (errno != EEXIST) {
             perror("mount");
             return -1;
         }
     }
-    printf("File system mounted successfully\n");
+    printf("Proc filesystem mounted successfully\n");
 
-    /*
-    pid_t pid = vfork();
-    if (pid == 0) {
-        printf("Child is running ...\n");
-        execl("/btp/sbin/procfs", "procfs", NULL);
-        exit(0);
-    } else {
-        int ret = 0;
-        waitpid(pid, &ret, 0);
-        printf("Parent gets code [%d]\n", ret);
-    }
-    */
+    display_net_dev();
+
     return 0;
 }
